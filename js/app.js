f430827c6111722157d12281624cfd1a97f46806
Brandon Pitts <brandomoki.bp@gmail.com>
2022-07-13 22:34:36 -0700

    Completed day3 lab

--- js/app.js
+++ js/app.js
@@ -1,55 +1,117 @@
-'use strict';
+// 'use strict';
 
-let siteVisitor = prompt('What is your name?');
+// let siteVisitor = prompt('What is your name?');
+// let score = 1;
+// alert(`Welcome to my site ${siteVisitor}! Guess these facts about me.`);
 
-alert(`Welcome to my site ${siteVisitor}! Guess these facts about me.`);
+// let questionOne = prompt('Yes or No...Do I have Children?').toLowerCase();
 
-let questionOne = prompt('Yes or No...Do I have Children?').toLowerCase();
+// if(questionOne === 'y' || questionOne === 'yes'){
+//   alert('You are correct');
+//   score++;
+// } else if(questionOne === 'n' || questionOne === 'no'){
+//   alert('You are wrong!');
+// } else{
+//   alert('Answer yes or no');
+// }
 
-if(questionOne === 'y' || questionOne === 'yes'){
-  alert('You are correct');
-} else if(questionOne === 'n' || questionOne === 'no'){
-  alert('You are wrong!');
-} else{
-  alert('Answer yes or no');
-}
+// let questionTwo = prompt('Have I ever jumped from an airplane?').toLowerCase();
 
-let questionTwo = prompt('Have I ever jumped from an airplane?').toLowerCase();
+// if(questionTwo === 'y' || questionTwo === 'yes'){
+//   alert('You are correct. I was a Paratrooper');
+//   score++;
+// } else if(questionTwo === 'n' || questionTwo === 'no'){
+//   alert('You are wrong!');
+// } else{
+//   alert('Answer yes or no');
+// }
 
-if(questionTwo === 'y' || questionTwo === 'yes'){
-  alert('You are correct. I was a Paratrooper');
-} else if(questionTwo === 'n' || questionTwo === 'no'){
-  alert('You are wrong!');
-} else{
-  alert('Answer yes or no');
-}
+// let questionThree = prompt('Was I ever in the Military?').toLowerCase();
 
-let questionThree = prompt('Was I ever in the Military?').toLowerCase();
+// if(questionThree === 'y' || questionThree === 'yes'){
+//   alert('You are correct. I was in th US Army');
+//   score++;
+// } else if(questionThree === 'n' || questionThree === 'no'){
+//   alert('You are wrong!');
+// } else{
+//   alert('Answer yes or no');
+// }
 
-if(questionThree === 'y' || questionThree === 'yes'){
-  alert('You are correct. I was in th US Army');
-} else if(questionThree === 'n' || questionThree === 'no'){
-  alert('You are wrong!');
-} else{
-  alert('Answer yes or no');
-}
+// let questionFour = prompt('Do I live in the desert?').toLowerCase();
 
-let questionFour = prompt('Do I live in the desert?').toLowerCase();
+// if(questionFour === 'y' || questionFour === 'yes'){
+//   alert('You are correct. I live in Las Vegas, NV');
+//   score++;
+// } else if(questionFour === 'n' || questionFour === 'no'){
+//   alert('You are wrong!');
+// } else{
+//   alert('Answer yes or no');
+// }
 
-if(questionFour === 'y' || questionFour === 'yes'){
-  alert('You are correct. I live in Las veags, NV');
-} else if(questionFour === 'n' || questionFour === 'no'){
-  alert('You are wrong!');
-} else{
-  alert('Answer yes or no');
-}
+// let questionFive = prompt('2+2=4? yes or no').toLowerCase();
 
-let questionFive = prompt('What is my age? YES ... NO .. ').toLowerCase();
+// if(questionFive === 'n' || questionFive === 'no'){
+//   alert('You are wrong!');
+//   score++;
+// } else if(questionFive === 'y' || questionFive === 'yes'){
+//   alert('WowZer! You really know your stuff.');
+// } else{
+//   alert('Answer yes or no');
+// }
 
-if(questionFive === 'y' || questionFive === 'yes' || questionFive === 'n' || questionFive === 'no'){
-  alert('You are wrong!');
-} else if(questionFive === '32'){
-  alert("By, Gum. You've done it.");
-} else{
-  alert('Answer yes or no');
-}
+// let attempts = 4;
+// const age = 32;
+// let questionSix;
+
+// while (attempts){
+//   questionSix = Number(prompt('Guess my Age between 25-35'));
+//   if (questionSix === age)
+//   {
+//     score++;
+//     alert('Great Scott!');
+//     break;
+//   } else
+//   {
+//     attempts--;
+//     if (attempts <= 0)
+//     {
+//       alert('Not the best guesser are we? I am ' + age + ' years old.');
+//     } else if (questionSix > age)
+//     {
+//       alert('Too high!');
+//     } else if (questionSix < age)
+//     {
+//       alert('Too low!');
+//     }
+//   }
+// }
+
+// attempts = 6;
+// let userResponse = score;
+// // let questionSeven;
+// let arrayOne = ['red', 'blue', 'black', 'green'];
+// while (attempts && userResponse === score){
+//   let questionSeven = prompt('What colors am I thinking of?').toLowerCase();
+
+//   for (let i = 0; i < arrayOne.length; i++)
+//   {
+//     if (arrayOne[i] === questionSeven){
+//       score++;
+//       break;
+//     }
+//   }
+//   if (userResponse === score){
+//     alert('Try again, sucka!');
+//   }
+// }
+// if (attempts === 0){
+//   alert('GameOver!!!');
+// }
+
+// alert('By gum, you\'ve done it!');
+
+// alert('All possible answers - ' + arrayOne);
+
+// alert('Congrats you got ' + score + ' answers correct');
+
+// alert('Congrats you got ' + score + ' answers correct');
